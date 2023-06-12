d1336646cc3578d107f80f3d584a7e011384b83e
SupperChicken <nguyenvanthien3290@gmail.com>
2023-06-12 15:54:40 +0700

    cau4

--- cau4/LAB_4.py
+++ cau4/LAB_4.py
@@ -1,20 +1,39 @@
 import requests
 import json
+
 requests.packages.urllib3.disable_warnings()
 baseUrl = "https://api.meraki.com/api/v1"
 API_key = "6bec40cf957de430a6f1f2baa056b99a4fac9ea0"
 org_id = "681155"
-def get_org():
-    url = baseUrl + f"/organizations/{org_id}/inventory/devices"
+
+def get_org_inv():
+    url = baseUrl + "/organizations/{}/inventory/devices".format(org_id)
+    
     header = {
-        "X-Cisco-Meraki-API-Key" : API_key
+    "Content-Type" : "application/json",
+    "Accept" : "application/json",
+    "X-Cisco-Meraki-API-Key" : API_key 
     }
-    response= requests.get(url,headers=header,verify=False)
-    data=response.json()
-    parse_data=json.dumps(data,indent=4)
-    print(parse_data)
-    # for i in data:
-    #     id_org=i["id"]
-    #     print(id_org)
+
+    respone = requests.get(url,headers=header)
+
+    data_inv =respone.json()
+
+    parse_data_inv =json.dumps(data_inv, indent=4)
+
+    print("cau_b:")
+    print(parse_data_inv)
+    print("-----------")
+
+
+    print("cau_c:")
+    deviceid_null = []
+    for i in data_inv:
+        if i["networkId"] == None:
+            deviceid_null.append(i)
+    print(json.dumps(deviceid_null, indent=4))
+    print ("-------")
+
+
 if __name__ == "__main__":
-    get_org()
+        get_org_inv()
